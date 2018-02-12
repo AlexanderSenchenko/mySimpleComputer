87b7235ee0b0314939b4d4a2fdd537bb9dd427ba
hegemonies <hegemonies@yandex.ru>
2018-02-12 19:11:24 +0700

    Fix

--- memory.c
+++ memory.c
@@ -15,7 +15,7 @@ int sc_memoryInit()
 
 int sc_memorySet(int address, int value)
 {
-	if (address >= SIZE - 1 || address < 0)
+	if (address >= SIZE || address < 0)
 		return 1;//выход за границы памяти
 
 	memory[address] = value;
@@ -25,7 +25,7 @@ int sc_memorySet(int address, int value)
 
 int sc_memoryGet(int address, int *value)
 {
-	if (address >= SIZE - 1 || address < 0)
+	if (address >= SIZE || address < 0)
 		return 1;//выход за границы памяти
 
 	*value = memory[address];
