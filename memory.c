b093cb5770c97fbcd9f04ac88617a6894ae63d75
Antihayp <zayzsacha@mail.ru>
2018-02-13 20:24:19 +0700

    Register

--- memory.c
+++ memory.c
@@ -1,10 +1,6 @@
 #include "memory.h"
 #include <stdio.h>
 
-#define SIZE 100
-
-int memory[SIZE];
-
 int sc_memoryInit()
 {
 	for (int i = 0; i < SIZE; i++) {
@@ -26,7 +22,7 @@ int sc_memorySet(int address, int value)
 int sc_memoryGet(int address, int *value)
 {
 	if (address >= SIZE || address < 0)
-		return 1;//выход за границы памяти
+		return 1;//выход за границы памяти (C)
 
 	*value = memory[address];
 
@@ -53,7 +49,22 @@ int sc_memoryLoad(char *filename)
 	return 0;
 }
 
+int sc_regInit()
+{
+	registr = 0;
+	return 0;
+}
 
+int sc_regSet(int value)
+{
+	registr = registr | value;
+	return 0;
+}
+
+int sc_regGet(int *value)
+{
+
+}
 
 int sc_memoryPrint()
 {
--- memory.h
+++ memory.h
@@ -1,14 +1,26 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#define SIZE 100
+
+#define A 0b00000001//переполнение при выполнение операции
+#define B 0b00000010//ошибка деления на 0
+#define C 0b00000100//ошибка выхода за границы массива
+#define F 0b00001000//игнорирование тактовых импульсов
+#define G 0b00010000//указана неверная команда
+
+int memory[SIZE];
+
+int registr;
+
 int sc_memoryInit();
 int sc_memorySet(int, int);
 int sc_memoryGet(int, int*);
 int sc_memorySave(char*);
 int sc_memoryLoad(char*);
-//int sc_regInit(void);
-//int sc_regSet(int, int);
-//int sc_regGet(int, int*);
+int sc_regInit(void);
+int sc_regSet(int);
+int sc_regGet(int*);
 //int sc_commandEncode(int, int, int*);
 //int sc_commandDecode(int, int*, int*);
 
